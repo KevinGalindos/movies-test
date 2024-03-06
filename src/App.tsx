43567e3b30d566a0c118d84bdf9747f2e25ce4af
Kevin Galindo <kevingalindos@gmail.com>
2024-03-06 11:55:54 -0500

    :construction: init app

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p>
+      react works!
+    </p>
+
+    </>
+  )
+}
+
+export default App
